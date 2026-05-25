af891c12a310ae97ae14197dbf102bdda23c2367
Navaneeth <navaneethkbiju2003@gmail.com>
2026-05-25 16:45:23 +0530

    Added task entity

---
+++ src/main/java/com/luminar/SpringbootMysqlThymeleafTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.luminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlThymeleafTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMysqlThymeleafTaskManagerApplication.class, args);
+	}
+
+}
